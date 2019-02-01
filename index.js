da08808bbdb4ed4a428c78dd4b6e07e3b5a971ac
ASHLEEN YOUN JI KIM <ashleenkim03@gmail.com>
2019-02-01 20:53:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ function happyHolidays () {
   return "Happy Holidays!"
 }
 
-function sayhappyHolidayTo () {
-  console.log('Happy' + 'holidays, ${name}!')
+function happyHolidayTo () {
+  return `Happy holidays, ${name}!`
 }
+
+function happyHolidayTo (holiday, name) {
+  return `It's ${holiday}, ${name}!`
+}
+
+function holodayCountdown (days, holidays) { return `It's ${days} days until ${holidays}`
+}
+
